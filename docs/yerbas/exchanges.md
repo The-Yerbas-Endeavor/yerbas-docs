c92f08efa98ef65009f8b9b59ae921073f768949
The-Yerbas-Endeavor <yerbascore@gmail.com>
2026-07-13 10:52:16 -0700

    3.2.0.15 Update

--- docs/yerbas/exchanges.md
+++ docs/yerbas/exchanges.md
@@ -6,14 +6,7 @@ Yerbas can be bought, sold or traded on a variety of platforms.
 
 ### YERB/USDT
 
-Xeggex.com Yerb/USDT https://xeggex.com/market/YERB_USDT
+NestEx.com Yerb/USDT https://trade.nestex.one/spot/YERB_USDT
 
-Xeggex.com Yerb/BTC https://xeggex.com/market/YERB_BTC
 
-## wYERB Bridges & Swaps
 
-### wYERB on BSC
-
-Yerbas Bridge: <https://wrap.yerbas.org/>
-
-PancakeSwap: <https://pancakeswap.finance/swap?outputCurrency=0x2100591c0b692c53a0E11cc328646309e6ea12eF&inputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56>
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -135,7 +135,7 @@ const config = {
       announcementBar: {
         id: 'new-wallet-release',
         content:
-          'Release <b>v3.1.4.20</b>: There is a new version of the wallet! Updating is required to keep your wallet functioning after the <a href="/docs/assets/" >Assets</a> release!',
+          'Release <b>v3.2.0.15</b>: There is a new version of the wallet! Updating is required to keep your wallet functioning ',
         backgroundColor: 'var(--ifm-color-danger)',
         textColor: 'var(--ifm-color-secondary)',
         isCloseable: true,
@@ -158,10 +158,6 @@ const config = {
               {
                 label: 'Discord',
                 href: 'https://discord.gg/XGEp2cKSKF',
-              },
-              {
-                label: 'Telegram',
-                href: 'https://t.me/Yerbas420',
               },
               {
                 label: 'Reddit',
